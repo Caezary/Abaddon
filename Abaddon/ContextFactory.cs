c8acccb910a1b6202adbd8cbd3eb919ee54338a1
Caezary <cezary.komarzeniec@gmail.com>
2019-06-08 11:20:44 +0200

    add context factory

---
+++ Abaddon/ContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Abaddon.Data;
+using Abaddon.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abaddon
+{
+    public class ContextFactory
+    {
+        public CurrentState<TEntry> CreateInitialState<TEntry>(
+            int width, int height, string values, Func<char, TEntry> converter)
+        {
+            if (width * height != values.Length)
+            {
+                throw new StateInitializationError();
+            }
+            var rows = values.Select((c, index) => new { index, value = converter(c) })
+                .GroupBy(x => x.index / width, x => x.value)
+                .Select(g => new BoardRow<TEntry>(g.ToList()))
+                .ToList();
+
+            var board = new Board<TEntry>(rows);
+
+            return new CurrentState<TEntry>(board);
+        }
+    }
+}
