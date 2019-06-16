3ead2de6dc090dbdb687c35eb09f05a358ff785e
Caezary <cezary.komarzeniec@gmail.com>
2019-06-17 01:22:11 +0200

    add instruction exceptions

--- Abaddon/Exceptions/AbaddonException.cs
+++ Abaddon/Exceptions/AbaddonException.cs
@@ -5,6 +5,8 @@ namespace Abaddon.Exceptions
     public class AbaddonException : Exception { }
     public class InstructionLimitReachedError : AbaddonException { }
     public class InstructionsMissingError : AbaddonException { }
+    public class UnknownInstructionError : AbaddonException { }
+    public class MalformedInstructionError : AbaddonException { }
 
     public class StateInitializationError : AbaddonException { }
 }
